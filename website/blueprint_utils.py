171d6b2dd2d4251799dc84db9da4c02567d8d0cb
houxh <houxuehua49@gmail.com>
2016-04-18 17:07:54 +0800

    登录路径

--- website/blueprint_utils.py
+++ website/blueprint_utils.py
@@ -39,13 +39,13 @@ def login_required(function=None, redirect_url_for=None):
             if 'user' in session and session['user'].get('id'):
                 if session['user']['role'] == 1:
                     if session['user'].get('email_checked') == 0:
-                        return redirect(url_for('web.register_valid'))
+                        return redirect(url_for('account.register_valid'))
                     else:
                         return f(*args, **kwargs)
                 elif session['user']['role'] == 2 or session['user']['role'] == 3:
                     return f(*args, **kwargs)
 
-            return redirect(url_for('web.login',
+            return redirect(url_for('account.login',
                                     redirect_url=url_for(redirect_url_for)
                                     if redirect_url_for else None))
 
